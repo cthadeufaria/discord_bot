46f304803d0d57dc824e0a4284fa8f259145fb44
cthadeufaria <47366883+cthadeufaria@users.noreply.github.com>
2021-01-31 00:45:13 -0300

    project structure

---
+++ botolino/webhook.py
@@ -0,0 +1,18 @@
+def webhook():
+    import requests
+    import discord
+    from discord import Webhook, RequestsWebhookAdapter, File
+    import os
+    print(os.getenv('discord_id'))
+    # Create webhook
+    webhook = Webhook.partial(os.getenv('discord_id'), os.getenv('discord_token'),\
+    adapter=RequestsWebhookAdapter())
+    
+    print(os.getenv('discord_id'))
+    # Send temperature as text
+    webhook.send('!play poze do rodo anos 80 II', username='Bot')
+    
+    # Upload image to server
+    # webhook.send(file=discord.File(&quot;latest_img.jpg&quot;))
+
+webhook()
