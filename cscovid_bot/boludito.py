6d5b681843537e7dd842a57c0ebc60ce83f1eae1
cthadeufaria <47366883+cthadeufaria@users.noreply.github.com>
2021-02-14 13:45:45 -0300

    new function calls

--- cscovid_bot/boludito.py
+++ cscovid_bot/boludito.py
@@ -1,12 +1,13 @@
 import discord, os, pafy, vlc, youtube_dl
 from discord.ext import commands
-from youtubesearchpython.__future__ import *
+from youtubesearchpython import VideosSearch
 
 bot = commands.Bot(command_prefix='$')
 
 boludos = {
     'DonHabraone#2093' : ['Victor', 1],
-    'J Cresta#1514' : ['Tonecito', 0]
+    'J Cresta#1514' : ['Tonecito', 0],
+    'carlosfaria#3773' : ['Charles', 0]
 }
 messages = {
     'hola' : [
@@ -16,29 +17,89 @@ messages = {
     ],
     'esp' : 
         '¡No entiendo, puto! Solo hablo español.',
+    'erro' :
+        ['No recordaré esta canción. Fumé mucha marihuana.']
 }
 cucaracha = 'https://www.youtube.com/watch?v=jp9vFhyhNd8'
 
 
+def play_music(voice_client):
+    if voice_client.is_playing() == True:
+        pass
+    else:
+        with youtube_dl.YoutubeDL() as ydl:
+            song_info = ydl.extract_info(cucaracha, download=False)
+        voice_client.play(
+            discord.FFmpegPCMAudio(song_info["formats"][0]["url"]), after = lambda e: print('done')
+        )
+
+
+async def clean_conn_voice(ctx, bot):
+    conn = 0
+    user = ctx.message.author
+    try:
+        channel = ctx.message.channel   # AttributeError if not in a voice channel.
+        voice_channel = user.voice.channel
+        for bot_vc in bot.voice_clients:
+            if voice_channel != bot_vc.channel:
+                bot_vc.stop()
+                await bot_vc.disconnect()
+            else:
+                conn = 1
+                voice_client = bot_vc
+        if conn == 0:
+            voice_client = await voice_channel.connect()
+        else:
+            pass
+    except AttributeError:
+        await channel.send(messages['hola'][2])
+        voice_client = None
+    return voice_client
+
+
 @bot.command()
 async def hola(ctx):
+    voice_client = await clean_conn_voice(ctx, bot)
+    if voice_client != None:
+        if boludos[str(user)][1] == 1:
+            await channel.send(messages['hola'][1])
+        else:
+            await channel.send(messages['hola'][0].format(str(user)))
+            play_music(voice_client)
+    else:
+        pass
+
+
+@bot.command()
+async def para(ctx):
+    for voice_client in bot.voice_clients:
+        voice_client.stop()
+        await voice_client.disconnect()
+
+
+@bot.command()
+async def tocar(ctx, *args):
+    search_str = ' '.join(args)
     user = ctx.message.author
     channel = ctx.message.channel
-    
     voice_channel = user.voice.channel
     voice_client = await voice_channel.connect()
     
-    with youtube_dl.YoutubeDL() as ydl:
-        song_info = ydl.extract_info(cucaracha, download=False)
-    
-    voice_client.play(
-        discord.FFmpegPCMAudio(song_info["formats"][0]["url"]), after=lambda e: print('done', e)
-    )
-
-
-    @bot.command()
-    async def para(ctx):
-        voice_client.stop()
+    if user in boludos.keys():
+        await channel.send(messages['hola'][1])
+    elif search_str == None:
+        await channel.send(messages['hola'][1])
+    else:
+        video_search = VideosSearch(search_str, limit=2)
+        # song_name = 
+        # song_url = 
+        print(videosSearch.result())
+        with youtube_dl.YoutubeDL() as ydl:
+            song_info = ydl.extract_info(cucaracha, download=False)
+        voice_client.play(
+            discord.FFmpegPCMAudio(song_info["formats"][0]["url"]), after=lambda e: print('done', e)
+        )
+        # await channel.send('Tocando ' + song_name)
 
 
 
