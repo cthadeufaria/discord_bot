3d8d2e39a60c35c44d4f4eafed7c4c7bf9f73b56
cthadeufaria <crls.thadeu@gmail.com>
2022-02-06 19:12:53 -0300

    random commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # discord_bot
+
+# bot events:
+    # @bot.event
+    # async def on_ready():
+    #     print('We have logged in as {0.user}'.format(bot))
+
+    # @bot.event
+    # async def on_message(message):
+    #     if message.author == bot.user:
+    #         return
+
+    #     # # excessão boludos
+    #     # if str(message.author) in boludos.keys():
+    #     #     await message.channel.send(messages['hola'][1])
+    #     # #
+
+    #     if message.content.startswith('oi'):
+    #         await message.channel.send(messages['esp'])
+
+    #     elif message.content.startswith('hola, manito'):
+    #         await message.channel.send(
+    #             messages['hola'][0].format(str(message.author))
+    #         )
--- boludito.py
+++ boludito.py
@@ -104,27 +104,5 @@ async def tocar(ctx, *args):
         pass
 
 
-# bot events:
-    # @bot.event
-    # async def on_ready():
-    #     print('We have logged in as {0.user}'.format(bot))
-
-    # @bot.event
-    # async def on_message(message):
-    #     if message.author == bot.user:
-    #         return
-
-    #     # # excessão boludos
-    #     # if str(message.author) in boludos.keys():
-    #     #     await message.channel.send(messages['hola'][1])
-    #     # #
-
-    #     if message.content.startswith('oi'):
-    #         await message.channel.send(messages['esp'])
-
-    #     elif message.content.startswith('hola, manito'):
-    #         await message.channel.send(
-    #             messages['hola'][0].format(str(message.author))
-    #         )
 
 bot.run(os.getenv('DISCORD_BOT_TOKEN'))
