0b1303b878faf2cc4526de240df4863c25cfb34e
carlosthadeufaria <crls.thadeu@gmail.com>
2022-02-15 11:50:55 -0300

    topc

--- cards.py
+++ cards.py
@@ -12,7 +12,7 @@ class Card:
         self.rank = rank
 
     def __str__(self):
-        return self.rank + ' of ' + self.suits
+        return self.rank + ' of ' + self.suit
 
 
 class Deck:
@@ -22,4 +22,88 @@ class Deck:
         for suit in suits:
             for rank in ranks:
                 self.deck.append(Card(suit, rank))
-                
+    
+    def __str__(self):
+        deck_comp = ''  # start with an empty string
+        for card in self.deck:
+            deck_comp += '\n '+card.__str__()  # add each Card object's print string
+        return 'The deck has:' + deck_comp
+
+    def shuffle(self):
+            random.shuffle(self.deck)
+
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
+
+# test_deck = Deck()
+# print(test_deck)
+
+
+class Hand:
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0   # start with zero value
+        self.aces = 0    # add an attribute to keep track of aces
+
+    def add_card(self, card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+        if card.rank == 'Ace':
+            self.aces += 1
+
+    def adjust_for_ace(self):
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
+            
+# test_deck = Deck()
+# test_deck.shuffle()
+# test_player = Hand()
+# test_player.add_card(test_deck.deal())
+# test_player.add_card(test_deck.deal())
+# test_player.value
+# print(test_player.value)
+# for card in test_player.cards:
+#     print(card)
+
+class Chips:
+    def __init__(self):
+        self.total = 100
+        self.bet = 0
+    
+    def win_bet(self):
+        self.total += self.bet
+    
+    def lose_bet(self):
+        self.total -= self.bet
+
+def take_bet(chips):
+    while True:
+        try:
+            chips.bet = int(input('How many chips would you like to bet? '))
+        except ValueError:
+            print('Sorry, a bet must be an integer')
+        else:
+            if chips.bet > chips.total:
+                print("sorry, your bet cannot exceed", chips.total)
+            else:
+                break
+
+def hit(deck, hand):
+    hand.add_card(deck.deal())
+    hand.adjust_for_ace()
+
+def hit_or_stand(deck, hand):
+    global playing #controls while loop
+    while True:
+        x = input('would you like to hit or stand? Enter "h" or "s" ')        
+        if x[0].lower == 'h':
+            hit(deck, hand)
+        elif x[0] == 's':
+            print('player stands. Dealer is playing')
+            playing = False        
+        else:
+            print('Sorry, please enter a valid response')
+            continue
+        break
